4ecfea26844d71ef191ed9063169e36614f3e007
Jonathan Hollingsworth <jonforest@gmail.com>
2020-04-06 06:17:44 +1200

    Initial commit

---
+++ app/data/talks.js
@@ -0,0 +1,14 @@
+export default {
+  '2020-04-09': {
+    meetingLink: 'https://tbd.com/123e24',
+    talks: [{
+      title: 'Introduction to Solid',
+      author: 'Fabian Cook',
+      description: 'Solid is an exciting new project led by Prof. Tim Berners-Lee, inventor of the World Wide Web, taking place at MIT. The project aims to radically change the way Web applications work today, resulting in true data ownership as well as improved privacy.'
+    }, {
+      title: 'Coding Exercise Problem Solving',
+      author: 'Sam Seay',
+      description: 'While applying for a job, Sam was presented with a coding exercise that contianer some entertaining problems. He\'s going to be telling us all about using IntersectionObserver and window.requestIdleCallback to solve them in exciting ways.'
+    }]
+  }
+}
---
+++ app/routes/home.js
@@ -0,0 +1,8 @@
+import Route from '@ember/routing/route';
+import data from '../data/talks'
+
+export default class MainRoute extends Route {
+  model () {
+    return data['2020-04-09']
+  }
+}
