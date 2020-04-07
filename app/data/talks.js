d426453398ca02d3fe4901ce3dd336b8ab755873
Jonathan Hollingsworth <jonforest@gmail.com>
2020-04-07 20:53:11 +1200

    Added talks and a few other bits and pieces

--- app/data/talks.js
+++ app/data/talks.js
@@ -1,11 +1,17 @@
 export default {
   '2020-04-09': {
-    meetingLink: 'https://tbd.com/123e24',
+    meetingLink: 'https://us02web.zoom.us/j/691023546',
     talks: [{
+      image: 'img/talks/solid.png',
+      imageAlt: 'Solid logo',
       title: 'Introduction to Solid',
       author: 'Fabian Cook',
-      description: 'Solid is an exciting new project led by Prof. Tim Berners-Lee, inventor of the World Wide Web, taking place at MIT. The project aims to radically change the way Web applications work today, resulting in true data ownership as well as improved privacy.'
+      description: 'Solid is an exciting new project led by Prof. Tim Berners-Lee, inventor of the World Wide Web, taking place at MIT. The project aims to radically change the way Web applications work today, resulting in true data ownership as well as improved privacy.',
+      links: ['https://solid.mit.edu/']
     }, {
+      image: 'img/talks/infinite.png',
+      imageAlt: 'Infinity symbol',
+      imageAttribution: 'Photo by Angely Acevedo on Unsplash',
       title: 'Coding Exercise Problem Solving',
       author: 'Sam Seay',
       description: 'While applying for a job, Sam was presented with a coding exercise that contianer some entertaining problems. He\'s going to be telling us all about using IntersectionObserver and window.requestIdleCallback to solve them in exciting ways.'
